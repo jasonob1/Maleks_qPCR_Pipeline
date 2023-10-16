cca7a376c8646194deff3b2e5502970591e5224c
OBrien <Jason.Obrien@ec.gc.ca>
2023-10-16 12:49:00 -0400

    Import Data in SCript

---
+++ script outline.R
@@ -0,0 +1,37 @@
+#### Initialize Graphical User Interface (GUI) using Shiny?? ####
+
+
+
+#### Libraries ####
+library(tidyverse)
+library(readxl)
+
+
+#### 1st step: Import Data #####
+
+# locate files
+setwd("Practice Data/2023-10-12 Practice/")
+allFiles<-list.files()
+metaDataFileName <- "metadata.xlsx"
+dataFiles <- allFiles[!allFiles %in% metaDataFileName]
+
+
+# load files
+metadata <- read_excel(metaDataFileName)
+
+rawData <- list()
+
+for(i in dataFiles){
+  rawData[[i]] <- read.table(file = i, header = TRUE, sep="\t", stringsAsFactors = FALSE, check.names=FALSE)
+}
+
+# combine files into a single table
+
+# A) combine all "rawData files"
+        # tidyverse "join" using "Well Name" column
+        # transpose?
+
+# B) "map" them to the metadata
+        # join using "SampleID" column (with some trickery)
+
+
