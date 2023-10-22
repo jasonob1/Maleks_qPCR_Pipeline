76cf7c7b999ffda91c83bf8631f30499b54a8981
MalekSmadi <malek.smadi@ec.gc.ca>
2023-10-22 16:17:50 -0400

    Update Oct 23 (Task) Questions.R

--- Practice Data/2023-10-12 Practice/Oct 23 (Task) Questions.R
+++ Practice Data/2023-10-12 Practice/Oct 23 (Task) Questions.R
@@ -2,6 +2,11 @@
 library(tidyverse)
 library(readxl)
 
+
+#I don't need to load dplyr because tidyverse is already loaded right?
+
+
+
 ####Task A: Combine All "rawData Files"####
     #locate files
 setwd("C:\\Users\\maloo\\OneDrive\\Desktop\\GitHub\\Maleks_qPCR_Pipeline\\Practice Data\\2023-10-12 Practice")
@@ -27,10 +32,16 @@ datafiles <- allfiles[!allfiles %in% metaDataFileName]
     #load files 
 metadata <- read_excel(metaDataFileName)
 rawData <- list()
-for(i in dataFiles){
+for(i in datafiles){
   rawData[[i]] <- read.table(file = i, header = TRUE, sep="\t", stringsAsFactors = FALSE, check.names=FALSE)
 }
 
   #Combine all "rawData Files"
 selected_columns <- map(rawData, ~ .x[["Well Name"]])
 combined_data2 <- bind_cols(selected_columns)
+
+columns_to_select<- c("Well Name")
+
+comb3<- rawData %>%
+        select(columns_to_select)
+
