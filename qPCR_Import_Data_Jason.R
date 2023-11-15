bd48eafa993ba8584001782ec28f22d0255b847c
jasonob1 <30319093+jasonob1@users.noreply.github.com>
2023-11-15 14:51:37 -0500

    load data and display merged Table

---
+++ qPCR_Import_Data_Jason.R
@@ -0,0 +1,136 @@
+# Libraries ----
+# check for installed libraries and install any missing ones
+list.of.packages <- c(
+  "tidyverse",
+  "readxl",
+  "ecotox",
+  "caret",
+  "sqldf",
+  "tcplfit2",
+  "drc",
+  "ggplot2",
+  "data.table",
+  "shiny",
+  "shinyscreenshot",
+  "refund.shiny",
+  "bslib"
+)
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# load libraries
+library(tidyverse)
+library(readxl)
+library(ecotox)
+library(caret)
+library(sqldf)
+library(tcplfit2)
+library(drc)
+library(ggplot2)
+library(data.table)
+library(shiny)
+library(shinyscreenshot)
+library(refund.shiny)
+library(bslib)
+
+
+# Define UI ----
+ui <- fluidPage(
+  theme = bs_theme(version = 4, bootswatch = "journal"),
+  titlePanel("QPCR App: Load Data"),
+  br(),
+  
+  # Instructions Button
+  actionButton("instrButton", "Instructions", style = "background-color: black; color: white;"),
+  br(),
+  br(),
+  
+  # Start a sidebar layout
+  sidebarLayout(
+    
+    # Load data buttons in Side Panel
+    sidebarPanel(
+      width = 4.5,
+      fileInput("metaFile", strong("Upload metadata.xlsx File:"),
+                accept = c(".xlsx")),
+      
+      fileInput("dataFiles", strong("Upload .txt File(s):"),
+                accept = c(".txt"),
+                multiple = TRUE),
+    ),
+    
+    # Render output$mergedTable in Main Panel
+    mainPanel(
+      tableOutput("mergedTable")
+    )
+  )
+)
+  
+
+# Define server logic ----
+server <- function(input, output) {
+  
+  # "Instructions" Button Behaviour
+  observeEvent(input$instrButton, {
+    showModal(modalDialog(
+      title = "Instructions",
+      p("Input 1: Upload a metadata.xlsx file with 3 columns (SampleID, Type, Control)"),
+      p("Input 2: Upload .txt files that match the name in metadata's SampleID column. Each .txt file has to contain 'Well Name' and 'Ct (dRn)' columns"),
+      easyClose = TRUE,
+      footer = tagList(
+        actionButton("instrCloseButton", "Close", style = "background-color: black; color: white;")),
+      size = "l",
+    ))
+    observeEvent(input$instrCloseButton, {
+      removeModal()
+    })
+  })
+  
+  # Outputs: Merged Raw Data Table ----
+  output$mergedTable <- renderTable ({
+    req(input$metaFile)
+    req(input$dataFiles)
+    metaPath <- input$metaFile$datapath
+    metadata <- read_excel(metaPath)
+    
+    rawList <- list()
+    for(i in 1:nrow(input$dataFiles)){
+      lname <- gsub(".txt", "", input$dataFiles$name[i])
+      rawList[[lname]] <- read.table(file = input$dataFiles$datapath[i], header = TRUE, sep="\t", stringsAsFactors = FALSE, check.names=FALSE, na.strings = "No Ct")
+      rawList[[lname]] <- rawList[[lname]][c("Well Name","Ct (dRn)")]
+      colnames(rawList[[i]])[2] <-lname
+    }
+    
+    # combine files into a single table
+    combData<- reduce(rawList, left_join, by = 'Well Name')
+    
+    # transpose ....but first move column 1 (well name) into row names
+    rownames(combData)<-combData$'Well Name'
+    combData <- combData %>%
+      dplyr::select(-'Well Name') %>%
+      t()
+    
+    # combine with metadata
+    # but first, put sample names back into a column
+    combData <- data.frame(SampleID=rownames(combData), combData)
+    
+    # join with metadata
+    fullData <- inner_join(metadata, combData, by="SampleID")
+  })
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+  
+
+
+
+  #### OVERALL APP FUNCTIONALITY #####
+  # LOAD DATA
+  # QUALITY CONTROL CHECK DATA
+  # NORMALIZE DATA
+  #   - house keeping gene normalization
+  #   - TMM normalization
+  # Differentially Expressed Gene analysis
+  # Principal Component Analysis (PCA)
+  # Heat Maps
