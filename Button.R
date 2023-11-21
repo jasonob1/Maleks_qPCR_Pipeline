f730bd7ead1b071cbc5f46cc7302347d913226b8
MalekSmadi <146832589+MalekSmadi@users.noreply.github.com>
2023-11-21 07:44:41 -0500

    Button and Page

---
+++ Button.R
@@ -0,0 +1,40 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Dynamic Buttons Example"),
+  
+    sidebarPanel(
+      # Initial button
+      actionButton("addButton", "Add Button"),
+      
+      # Rendered UI for dynamic buttons
+      uiOutput("dynamicButtons"),
+  )
+)
+
+server <- function(input, output, session) {
+  # Track the number of button clicks
+  clickCount <- reactiveVal(0)
+  
+  # Render additional buttons based on click event
+  output$dynamicButtons <- renderUI({
+    numButtons <- clickCount()
+    
+    # Create a list of buttons
+    buttons <- lapply(1:numButtons, function(i) {
+      actionButton(paste0("dynamicButton", i), paste("Button", i))
+    })
+    
+    # Return the list of buttons
+    tagList(buttons)
+  })
+  
+  # Increment click count when the initial button is clicked
+  observeEvent(input$addButton, {
+    clickCount(clickCount() + 1)
+  })
+  
+}
+
+shinyApp(ui=ui, server=server)
+
